fddaf7fd98f8034ed7322f2aa4d57e20a6e5ed75
Ravi Dudhagra <rdcosmicwonders@gmail.com>
2019-09-23 13:14:01 -0400

    removed sudo when installing forever

--- aws_scripts/build_dependencies.sh
+++ aws_scripts/build_dependencies.sh
@@ -2,7 +2,7 @@
 npmmodules=(express hbs)
 
 cd "$(dirname "$0")"/../source
-sudo npm install forever -g #lets the webserver run in the background
+npm install forever -g #lets the webserver run in the background
                             #as a daemon
 
 for package in ${npmmodules[@]}; do
